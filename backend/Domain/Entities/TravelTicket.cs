24ebf68f90519018995a074cb11d102232ba9643
Fabri1196 <fabripeirano@gmail.com>
2023-05-19 18:38:03 -0300

    Adding packages

--- backend/Domain/Entities/TravelTicket.cs
+++ backend/Domain/Entities/TravelTicket.cs
@@ -1,16 +1,18 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace Domain.Entity
+namespace Domain.Entities
 {
-    public class TravelTicket : entity
+    public class TravelTicket : Entity
     {
         public ICollection from { get; set; }
 
-        public ICollection from { get; set; }
+        public ICollection to { get; set; }
 
         public Date date { get; set; }
 
